c6402ae9292e02c4e0b981b495159bf17f7530ca
Hetvi Vaja <hetvivaja09@outlook.com>
2026-07-11 01:45:06 +0530

    Now Finally Completed Step-2 with Database Created.

---
+++ backend/leave/models/leave_model.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Leave(models.Model):
+
+    #Leave Type
+    LEAVE_TYPE_CHOICE=[
+        ('casual','Casual Leave'),
+        ('sick','Sick Leave'),
+        ('earned','Earned Leave'),
+        ('maternity','Maternity Leave'),
+    ]
+
+    #Status Choice
+    STATUS_CHOICE=[
+        ('pending',"Pending"),
+        ('approved','Approved'),
+        ('rejected','Rejected'),
+    ]
+
+    #Fields
+    employee=models.ForeignKey(User,on_delete=models.CASCADE,related_name='leaves')
+    leave_type=models.CharField(max_length=20,choices=LEAVE_TYPE_CHOICE)
+    start_date=models.DateField()
+    end_date=models.DateField()
+    reason=models.TextField()
+    status=models.CharField(max_length=20,choices=STATUS_CHOICE,default='pending')
+    applied_on=models.DateTimeField(auto_now_add=True)
+    update_on=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.employee.username} - {self.leave_type} ({self.status})"
